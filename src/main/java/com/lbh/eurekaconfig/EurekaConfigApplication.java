55ed1b2bdd9bcd3973bc6777010907fa77918799
liubinghao <advancer_lbh@hotmail.com>
2019-01-24 17:44:02 +0800

    Initial commit

---
+++ src/main/java/com/lbh/eurekaconfig/EurekaConfigApplication.java
@@ -0,0 +1,14 @@
+package com.lbh.eurekaconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConfigApplication.class, args);
+    }
+
+}
+
